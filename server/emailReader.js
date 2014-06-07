7279377c4a6dcad629506e65c6e7cb9b4f731151
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2014-06-07 17:44:44 +0200

    Reading real messages and passing on to websockets

--- server/emailReader.js
+++ server/emailReader.js
@@ -1,5 +1,6 @@
 var yaml_config = require('node-yaml-config');
 var ContextIO = require('contextio');
+var _ = require('lodash');
 
 var config = yaml_config.load(__dirname + '/contextio.yml');
 
@@ -8,11 +9,12 @@ var ctxioClient = new ContextIO.Client({
 	secret: config.contextIo.secret
 });
 
-exports.readEmail = function() {
+exports.readEmail = function(callback, callbackParameter) {
 
-	ctxioClient.accounts().get({limit:15}, function (err, response) {
+	ctxioClient.accounts('53932932facadd1674f0da85').messages().get({limit:15}, function (err, response) {
 	    if (err) throw err;
-	    console.log(response.body);
+	    // console.log(response.body);
+	    callback(response.body, callbackParameter);
 	});
 
 }
--- server/mapper.js
+++ server/mapper.js
@@ -3,37 +3,34 @@ var moment = require('moment');
 var http = require('http');
 
 var emailReader = require('./emailReader.js');
-emailReader.readEmail();
 
-var contextIoData = {
-	messages: [
-		{
-			read: false,
-			from: 'acolleague@thoughtworks.com',
-			date: moment(1402057473).format('YYYY-MM-DD')
-		},
-		{
-			read: false,
-			from: 'susanne@justsoftwareag.com',
-			date: moment(1402057473).format('YYYY-MM-DD')
-		}
-	]
+exports.readEmail = function(callWhenDone) {
+	emailReader.readEmail(mapEmailDataToRectangles, callWhenDone);	
 };
+
 
 // alle 10 Sekunden context.io fragen
 // query mit date_after = das letzte Mal als ich gefragt habe
 
 
-exports.mapEmailData = function() {
+function mapEmailDataToRectangles(messages, callback) {
+
+	// addresses.from.email
+	// addresses.from.name
 
 	function isFromThoughtworks(message) {
-		return message.from.indexOf('thoughtworks.com') > -1;
+		return message.addresses.from.email.indexOf('thoughtworks') > -1;
 	}
 
-	var countInternal = _.countBy(contextIoData.messages, isFromThoughtworks);
-	var internalMessages = countInternal.true;
-	var externalMessages = countInternal.false;
+	console.log('Got emails from', _.map(messages, function(message) {
+		return message.addresses.from.email;
+	}));
 
+	var countInternal = _.countBy(messages, isFromThoughtworks);
+
+	var internalMessages = countInternal.true || 0;
+	var externalMessages = countInternal.false || 0;
+	
 	var internalMessagesRect = { color: "blue" };
 	if(internalMessages < 5) {
 		internalMessagesRect.size = "small";
@@ -52,6 +49,6 @@ exports.mapEmailData = function() {
 		externalMessagesRect.size = "large";
 	}
 
-	return [ internalMessagesRect, externalMessagesRect ];
+	callback([ internalMessagesRect, externalMessagesRect ]);
 
 };
