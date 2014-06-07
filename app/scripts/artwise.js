0daff2fa6426bafe2fc243d80b4362af4f1c4be0
jonasvdheyden <jonas.vdheyden@googlemail.com>
2014-06-07 18:16:09 +0200

    blup

--- app/scripts/artwise.js
+++ app/scripts/artwise.js
@@ -11,36 +11,49 @@
 // columns = today, yesterday, etc.
 // row position not mapped yet
 
-function sketchProc(P) {
-console.log('executing...')
- P.setup = function() {
+
+function Mondrian() {
+  console.log('drawin Mondrian')
+
+  P.setup = function() {
     P.size(300,300);
-    P.background(255);
+    P.background(255); // background is white
     P.smooth();
   }  
 
- P.draw = function() {
+  P.draw = function() {
     P.stroke(0,0,0);
     P.strokeWeight(5);
-// Lineas Horizontales
-    P.line(0,60,300,60)   ;
+  // horizontal lines
+    P.line(0,60,300,60);
     P.line(0,120,300,120);
     P.line(0,200,300,200);
     P.line(0,260,300,260);
  
-//vertical lines
+  //vertical lines
     P.line(40,0,40,300);
     P.line(100,0,100,300);
     P.line(160,0,160,300);
     P.line(260,0,260,300);
  
-//horizontal lines
-    P.fill(230,120,20);
-    P.rect(40,60,60,60);
-    P.fill(20,89,45);
+  // rectangles
+    // rectangle for internal email today
+    P.fill(230,120,20);  // color is green
+    P.rect(40,60,60,60); // position should be in rightmost column
+    // rectangle for external email today
+    P.fill(20,89,45);  
     P.rect(100,120,60,80);
+    // rectangle for internal email yesterday
+    P.fill(45,45,140);
+    P.rect(260,120,100,80); 
+    // rectangle for external email yesterday
     P.fill(45,45,140);
     P.rect(260,120,100,80); 
   }
 
 }
+
+function sketchProc(P) {
+  Mondrian();
+}
+ 
