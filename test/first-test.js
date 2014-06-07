35d91986f334a562208881328531f1e99066b2d4
Birgitta Boeckeler <bboeckel@thoughtworks.com>
2014-06-07 14:36:43 +0200

    Starting setup with a NodeJS Websocket server and Karma tests

---
+++ test/first-test.js
@@ -0,0 +1,9 @@
+/* global describe, it */
+
+describe('Give it some context', function () {
+    describe('maybe a bit more context here', function () {
+        it('should run here few assertions', function () {
+
+        });
+    });
+});
