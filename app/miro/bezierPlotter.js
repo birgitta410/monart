e172ae47d0de6cc16a1a743396daac0a4ea0bc23
Birgitta B <bboeckel@thoughtworks.com>
2015-05-01 16:43:31 +0200

    Draw curve with processing, plot circles with bezier plotter

--- app/miro/bezierPlotter.js
+++ app/miro/bezierPlotter.js
@@ -1,46 +1,7 @@
-/*******************************************************/
-/*****************[ BezierPlotter.as ]*******************
+
+/*****************[ Migrated from BezierPlotter.as ]*******************
 
  Created: 09/25/02 - Paul Spitzer - www.paulspitzer.com
-
- Use: Create a new instance of the BezierPlotter class
- instanceName = new BezierPlotter(p1, p2, p3, p4);
- p1, p2, p3, p4 can be either MovieClips or Objects
- with _x and _y properties.
-
- Example 1: Using MovieClips - the parameters are the
- instance names of your MovieClips...
- BezierPlotter(mc1, mc2, mc3, mc4);
-
- Example 2: Using Objects - the parameters are Objects
- with _x and _y properties...
- obj1 = {_x: 100, _y: 100};
- obj2 = {_x: 100, _y: 400};
- obj3 = {_x: 400, _y: 400};
- obj4 = {_x: 400, _y: 100};
- BezierPlotter(obj1, obj2, obj3, obj4);
-
- Calling Methods: There are three methods which you
- have access to.
-
- definePoint(index, pos): Used to define the location
- of anyone of the four required points.  This method
- takes two parameters.  Index - describes which point
- you are defining.  Valid values are integers 1 - 4.
- Pos - is either a MovieClip or an Object with x and
- y values.
-
- setupProps(speed): Used to define properties used in
- the plotting equation.  For now the only parameter is
- speed.  Speed can be thought of as the number of points
- to plot.
-
- plotPoints(): Creates the array "points" which stores
- the x and y positions of the plotted points. The
- array is in the following structure: points[i][0] = X,
- points[i][1] = Y. Where i = an integer from 0 to speed
- or points.length.
-
 
  ********************************************************/
 
@@ -84,47 +45,4 @@ BezierPlotter.prototype.plotPoints = function()
   }
   this.points = ary_return;
   return this.points;
-};
-
-function getBezRand(){
-  var gBezMaxRand = 300;
-  return (Math.random()*gBezMaxRand)-gBezMaxRand/2;
-}
-
-function doBezTrail(startX, startY, trailTarget) {
-
-  var startPt = {x: startX, y: startY};
-
-  var ctrl1x = getBezRand();
-  var ctrl1y = getBezRand();
-  var ctrl2x = getBezRand();
-  var ctrl2y = getBezRand();
-
-  var ctrl1 = {x: startPt.x + ctrl1x, y: startPt.y + ctrl1y};
-  var ctrl2 = {x: trailTarget.x + ctrl2x, y: trailTarget.y + ctrl2y};
-
-  var endPt = {x: trailTarget.x, y: trailTarget.y};
-
-  var bezPlot = new BezierPlotter(startPt, ctrl1, ctrl2, endPt);
-  bezPlot.setupProps(30);
-
-  var plottedPoints = bezPlot.plotPoints();
-  console.log('bezier control points factors', ctrl1x, ctrl1y, ctrl2x, ctrl2y);
-  console.log('number of points', plottedPoints.length);
-
-  return {
-    points: plottedPoints,
-    start: startPt,
-    end: endPt,
-    ctrl1: {
-      x: ctrl1x,
-      y: ctrl1y
-    },
-    ctrl2: {
-      x: ctrl2x,
-      y: ctrl2y
-    }
-  };
-
-
 };
--- app/miro/sketch.js
+++ app/miro/sketch.js
@@ -5,32 +5,82 @@ function Miro(P) {
 
   var trail;
 
-  function drawTrail() {
-    P.stroke(0, 0, 0);
-    _.each(trail.points, function(point, i) {
-      if(trail.points.length > i+1) {
-        P.line(point[0], point[1], trail.points[i+1][0], trail.points[i+1][1]);
+  function getBezRand(){
+    var gBezMaxRand = 300;
+    return (Math.random()*gBezMaxRand)-gBezMaxRand/2;
+  }
+
+  function doBezTrail(startX, startY, trailTarget) {
+
+    var startPt = {x: startX, y: startY};
+
+    var ctrl1x = getBezRand();
+    var ctrl1y = getBezRand();
+    var ctrl2x = getBezRand();
+    var ctrl2y = getBezRand();
+
+    var ctrl1 = {x: startPt.x + ctrl1x, y: startPt.y + ctrl1y};
+    var ctrl2 = {x: trailTarget.x + ctrl2x, y: trailTarget.y + ctrl2y};
+
+    var endPt = {x: trailTarget.x, y: trailTarget.y};
+
+    var bezPlot = new BezierPlotter(startPt, ctrl1, ctrl2, endPt);
+    bezPlot.setupProps(50);
+
+    var plottedPoints = bezPlot.plotPoints();
+    console.log('bezier control points factors', ctrl1x, ctrl1y, ctrl2x, ctrl2y);
+    console.log('number of points', plottedPoints.length);
+
+    return {
+      points: plottedPoints,
+      start: startPt,
+      end: endPt,
+      ctrl1: {
+        x: ctrl1.x,
+        y: ctrl1.y
+      },
+      ctrl2: {
+        x: ctrl2.x,
+        y: ctrl2.y
       }
+    };
+
+  }
+
+  function drawControlPoints() {
+
+    P.fill(255, 0, 0);
+    _.each(trail.points, function(point) {
+      P.rect(point[0], point[1], 5, 5);
     });
+    function orZero(value) {
+      return value > 0 ? value : 0;
+    }
+
+    P.rect(orZero(trail.ctrl1.x), orZero(trail.ctrl1.y), 5, 5);
+    P.rect(orZero(trail.ctrl2.x), orZero(trail.ctrl2.y), 5, 5);
+
+    P.noFill();
   }
 
   P.setup = function() {
 
-    P.background(255);
     P.smooth(8);
     P.size(900, 700);
 
     trail = doBezTrail(200, 100, { x: 500, y: 400});
-    P.stroke(255, 102, 0);
-    // Processing's bezier draws a different line than our plotted trail...
-    P.bezier(trail.start.x, trail.start.y, trail.ctrl1.x, trail.ctrl1.y, trail.ctrl2.x, trail.ctrl2.y, trail.end.x, trail.end.y);
 
-    drawTrail();
   };
 
   var index = 0;
 
   P.draw = function() {
+
+    P.background(255);
+    P.stroke(0, 0, 0);
+    P.bezier(trail.start.x, trail.start.y, trail.ctrl1.x, trail.ctrl1.y, trail.ctrl2.x, trail.ctrl2.y, trail.end.x, trail.end.y);
+
+    drawControlPoints();
 
     index ++;
     if(trail.points.length <= index) {
