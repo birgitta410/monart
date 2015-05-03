a70653dcf63b64e92734d54548698893575e4f06
Birgitta B <bboeckel@thoughtworks.com>
2015-05-03 20:04:54 +0200

    Miro blue as processing sketch, escaping css hell

---
+++ app/miro/blue/sketch.js
@@ -0,0 +1,95 @@
+
+function MiroBlue(P, model) {
+  console.log('drawing MiroBlue');
+
+  var images;
+
+  var COLORS = {
+    red: {r: 198, g: 45, b: 39},
+    black: {r: 0, g: 0, b: 0},
+    white: {r: 255, g: 255, b: 255},
+    green: {r: 91, g: 143, b: 68},
+    blue: {r: 60, g: 67, b: 137},
+    yellow: {r: 244, g: 214, b: 64}
+  };
+
+  function color(pFct, color) {
+    pFct(color.r, color.g, color.b);
+  }
+
+  P.setup = function() {
+
+    P.smooth(8);
+    P.size(700*1.6, 700, P.P2D);
+
+    images = {
+      background: P.loadImage('images/background.png', 'png'),
+      strokes: {
+        red: P.loadImage('images/stroke_red.png', 'png'),
+        black: P.loadImage('images/stroke_black.png', 'png'),
+        white: P.loadImage('images/stroke_white.png', 'png')
+      },
+      stones: {
+        red: {
+          small: P.loadImage('images/small_red.png', 'png'),
+          medium: P.loadImage('images/medium_red.png', 'png'),
+          large: P.loadImage('images/large_red.png', 'png')
+        },
+        black: {
+          small: P.loadImage('images/small_black.png', 'png'),
+          medium: P.loadImage('images/medium_black.png', 'png'),
+          large: P.loadImage('images/large_black.png', 'png')
+        },
+        white: {
+          small: P.loadImage('images/small_white.png', 'png'),
+          medium: P.loadImage('images/medium_white.png', 'png'),
+          large: P.loadImage('images/large_white.png', 'png')
+        }
+      }
+    };
+  };
+
+  P.draw = function() {
+
+    P.background(255);
+    P.image(images.background, 0, 0);
+    images.strokes.red.resize(200, 600);
+    P.image(images.strokes.red, 50, 50);
+
+    var offset = 250;
+    _.each(model.history, function(entry) {
+      var stone = images.stones[entry.color][entry.size];
+      P.image(stone, offset, 590 - stone.height);
+      offset += stone.width + 50;
+    });
+
+  };
+
+}
+
+var miroModel = {
+  history: [
+    {size: 'small', color: 'black' },
+    {size: 'large', color: 'white' },
+    {size: 'small', color: 'black' }
+  ]
+};
+
+function sketchProc(P) {
+  MiroBlue(P, miroModel);
+}
+
+new Processing('miroCanvas', sketchProc);
+
+function processNewData(historyData) {
+  miroModel.history = historyData.history;
+}
+
+var wsHost = 'ws://' + window.location.host;
+var ws = new WebSocket(wsHost + '/miroBlue');
+
+ws.onmessage = function (event) {
+  artwise.processMessage(event, 'miroBlue', processNewData);
+};
+
+artwise.initPing(ws, function() { console.log('no connection!'); });
